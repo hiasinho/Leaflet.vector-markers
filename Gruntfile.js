1b586517b17388c183f55ce8f3d252e092c6f157
Mathias Schneider <hiasinho@me.com>
2015-01-23 07:34:00 +0100

    Ready for bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,6 @@
-// var pkgjson = require('./package.json');
- 
-// var config = {
-//   pkg: pkgjson,
-//   app: 'src',
-//   dist: 'dist'
-// }
- 
-// module.exports = function (grunt) {
- 
-//   // Configuration
-//   grunt.initConfig({
-//     config: config,
-//     pkg: config.pkg,
-//     bower: grunt.file.readJSON('./.bowerrc'),
-//     copy: {
-//       dist: {
-//        files: [{
-//          expand: true,
-//          cwd: '<%= config.app %>/_lib/font-awesome',
-//          src: 'css/font-awesome.min.css',
-//          dest: '<%= config.dist %>'
-//        },
-//        {
-//          expand: true,
-//          cwd: '<%= config.app %>/_lib/font-awesome',
-//          src: 'fonts/*',
-//          dest: '<%= config.dist %>'
-//        }]
-//       }
-//     },
-//     uglify: {
-//       options: {
-//         banner: '/*! <%= pkg.name %> lib - v<%= pkg.version %> -' +
-//           '<%= grunt.template.today("yyyy-mm-dd") %> */'
-//       },
-//       dist: {
-//         files: {
-//           '<%= config.dist %>/js/lib.min.js': [
-//             '<%= bower.directory %>/jquery/jquery.js',
-//             '<%= bower.directory %>/underscore/underscore.js',
-//             '<%= bower.directory %>/requirejs/require.js',
-//           ]
-//         }
-//       }
-//     }
-//   });
- 
-//   grunt.loadNpmTasks('grunt-contrib-copy');
-//   grunt.loadNpmTasks('grunt-contrib-uglify');
-//   grunt.loadNpmTasks('grunt-contrib-coffee');
- 
-//   grunt.registerTask('default', [
-//     'copy',
-//     'uglify'
-//   ]);
-// };
-
-
 'use strict';
 var packagejson = require('./package.json');
- 
+
 var config = {
   pkg: packagejson,
   app: 'src',
@@ -67,7 +8,7 @@ var config = {
 };
 
 module.exports = function (grunt) {
- 
+
   // Configuration
   grunt.initConfig({
     config: config,
@@ -98,7 +39,7 @@ module.exports = function (grunt) {
 
     sass: {
       dist: {
-        options: { 
+        options: {
           style: 'expanded'
         },
         files: {
@@ -107,14 +48,14 @@ module.exports = function (grunt) {
       }
     }
   });
-  
+
   grunt.registerTask('default', [
     'coffee_jshint',
     'coffee',
     'uglify',
     'sass'
   ]);
-  
+
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-coffee');
